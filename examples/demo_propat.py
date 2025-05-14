054871022c0a4c7603ebf41ab32f30fd09585abd
Kwan0Kim <166795744+Kwan0Kim@users.noreply.github.com>
2025-05-14 20:20:38 +0900

    Update demo_propat.py

--- examples/demo_propat.py
+++ examples/demo_propat.py
@@ -1,10 +1,7 @@
-import numpy as np
-from orbit import *
-from ADCS import *
-from kinematics import *
-from scipy import integrate
+from core.orbit import *
+from core.kinematics import *
+from core.control import *
 import matplotlib.pyplot as plt
-import copy
 from scipy.integrate import solve_ivp
 
 # demo)propat
@@ -15,32 +12,32 @@ from scipy.integrate import solve_ivp
 kepel = np.array([7000000, 0.01, 98, 0, 0, 0])
 
 # Orbit state vector:
-stat = kepel_statvec(kepel)
+stat = Orbit.kepel_statvec(kepel)
 
 # Attitude elements in Euler angles of a 3-1-3 (z-x-z) rotation
 eulzxz = np.array([30, 50, 20])*np.pi/180
 
 # Attitude in quaternions
-quat = ezxzquat(eulzxz)
+quat = Kinematics.ezxzquat(eulzxz)
 
 # Angular velocity vector in body frame:
 w_ang = np.array([0.1, 0, 0.5])           # in radians/sec
 
 # Compute the variations in keplerian elements due to the Earth oblateness
-delk = delkep(kepel)
+delk = Orbit.delkep(kepel)
 
 year = 2009
-mjd = djm(13, 4, year)     # Format (day, month, year)
-mjdo = djm(1, 1, year)     # modified julian date of 1/1/year
-mjd1 = djm(1, 1, year+1)   # modified julian date of 1/1/(year+1)
+mjd = Orbit.djm(13, 4, year)     # Format (day, month, year)
+mjdo = Orbit.djm(1, 1, year)     # modified julian date of 1/1/year
+mjd1 = Orbit.djm(1, 1, year+1)   # modified julian date of 1/1/(year+1)
 year_frac = year + (mjd - mjdo)/(mjd1 - mjdo)  # year and fraction
 
 # Ephemerides time:
-dfra = time_to_dayf(10, 20, 0)    # UTC time in (hour, minute, sec)
+dfra = Orbit.time_to_dayf(10, 20, 0)    # UTC time in (hour, minute, sec)
 
 # Propagation time in seconds:
 tstart = 0     # initial time (sec)
-tstep = 1    # step time (sec)
+tstep = 0.5    # step time (sec)
 tend = 600     # end time (10 minutes)
 
 # Inertia matrix of axis-symetric rigid body:
@@ -70,7 +67,7 @@ for t in np.arange(tstart, tend+tstep, tstep):
 	kep2 = kepel + delk * t
 
 	# To	convert from keplerian elements to state vector( if needed)
-	stat = kepel_statvec(kep2)
+	stat = Orbit.kepel_statvec(kep2)
 
 	# Perturbation torques:
 	ambt = np.array([0, 0, 0])
@@ -87,22 +84,22 @@ for t in np.arange(tstart, tend+tstep, tstep):
 	# Numeric integration (ODE45)
 	if flag_mag == 0:
 		def func(t, x, ext_torque=ext_torq, tensin=iner, teninv=invin):
-			return rigidbody_dynamics(t, x, ext_torque, tensin, teninv)
+			return Control.rigidbody_dynamics(t, x, ext_torque, tensin, teninv)
 		# print(func(t, att_vec))
 		sol = solve_ivp(func, (t, t+tstep), tuple(att_vec), rtol=1e-12, atol=1e-12)
 		Y = sol.y
 
 	else:
 		# To convert from inertial state vector to terrestrial vector
-		geoc = inertial_to_terrestrial(gst(mjd, dfra + t), stat.squeeze(0))
+		geoc = Orbit.inertial_to_terrestrial(Orbit.gst(mjd, dfra + t), stat.squeeze(0))
 		# Earth's magnetic field
-		sphe = rectangular_to_spherical(geoc)
+		sphe = Kinematics.rectangular_to_spherical(geoc)
 		alt = sphe[2] / 1000
 		elong = sphe[0]
 		colat = np.pi/2 - sphe[1]
-		earth_field = 1E-9*igrf_field(year_frac, alt, colat, elong)
+		earth_field = 1E-9*Orbit.igrf_field(year_frac, alt, colat, elong)
 		def func(t, x, ext_torq, iner, invin, mag_mom, earth_field):
-			return rigidbody_dynamics(t, x, ext_torq, iner, invin,	mag_mom, earth_field)
+			return Control.rigidbody_dynamics(t, x, ext_torq, iner, invin,	mag_mom, earth_field)
 		sol = solve_ivp(func, (t, t + tstep), tuple(att_vec.squeeze(0)), rtol=1e-12, atol=1e-12)
 		Y = sol.y
 
@@ -110,14 +107,13 @@ for t in np.arange(tstart, tend+tstep, tstep):
 	quat = np.array(att_vec[:4])		# propagated quaternion
 	w_ang = att_vec[4:7]		# propagated angular velocity
 
-	eulzxz = quatezxz(quat)		# euler angles
-
+	eulzxz = Kinematics.quatezxz(quat)		# euler angles
 	# attitude control torque logic (if any)
 	cont_torq = np.array([0, 0, 0])
 
 	# Store data to be plotted
 	time = np.concatenate([time, [t]], 0)
-	euler = np.concatenate([euler, eulzxz*180/np.pi], 1)
+	euler = np.concatenate([euler, np.transpose(eulzxz)*180/np.pi], 1)
 	omeg = np.concatenate([omeg, np.transpose([w_ang])], 1)
 	orbit = np.concatenate([orbit, np.transpose(stat)], 1)
 	keorb = np.concatenate([keorb, np.transpose([kep2])], 1)
@@ -143,19 +139,19 @@ plt.legend()
 
 plt.figure()
 plt.subplot(3, 1, 1)
-plt.plot(time, keorb[3, :], 'b',)
+plt.plot(time, keorb[3, :]*180/np.pi, 'b',)
 plt.xlabel('Time (s)')
 plt.ylabel('Ascencion node (deg)')
 plt.title('Right ascencion of the ascending node')
 
 plt.subplot(3, 1, 2)
-plt.plot(time, keorb[4, :], 'b')
+plt.plot(time, keorb[4, :]*180/np.pi, 'b')
 plt.xlabel('Time (s)')
 plt.ylabel('Perigee argument (deg)')
 plt.title('Perigee argument')
 
 plt.subplot(3, 1, 3)
-plt.plot(time, keorb[5, :], 'b')
+plt.plot(time, keorb[5, :]*180/np.pi, 'b')
 plt.xlabel('Time (s)')
 plt.ylabel('Mean anomaly (deg)')
 plt.title('Mean anomaly')
@@ -180,11 +176,4 @@ plt.title('Satellite velocity')
 plt.legend()
 
 plt.show()
-
-
-
-
-
-
-
 
